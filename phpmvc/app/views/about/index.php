17d0481bb9e226362397b507096391a0d94c53c1
Fmanik7 <133849033+Fmanik7@users.noreply.github.com>
2024-06-25 13:21:37 +0700

    Add files via upload

---
+++ phpmvc/app/views/about/index.php
@@ -0,0 +1,7 @@
+<div class="container">
+    
+    <h1 class="mt-4">About Me</h1>
+    <img src="<?= BASEURL; ?>/img/Feri.jpg" alt="Feri Manik" widht="200" height="300" class="rounded-circle shadow">
+    <p>Halo, nama saya <?= $data['nama']; ?>, umur saya <?=  $data['umur']; ?>, saya adalah seorang <?= $data['pekerjaan']; ?></p>
+
+</div>
